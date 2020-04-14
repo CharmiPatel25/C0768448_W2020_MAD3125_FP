0a3872876f3c4337bec4beb475f414d56c59432d
Charmi <c0768448@mylambton.ca>
2020-04-14 19:12:48 -0400

    Add Internet Class attributes

--- app/src/main/java/Model/Internet.java
+++ app/src/main/java/Model/Internet.java
@@ -1,4 +1,37 @@
 package Model;
 
-public class Internet {
+import java.util.Date;
+
+public class Internet extends  Bill {
+    private String internetProviderName;
+    private Double internetGbUsed;
+
+    public Internet(String billID, Date billDate, BillType billType, String internetProviderName, Double internetGBUsed) {
+        super(billID, billDate, billType);
+        this.internetProviderName = internetProviderName;
+        this.internetGbUsed = internetGBUsed;
+        this.billAmount = calculateBill();
+    }
+
+    public String getInternetProviderName() {
+        return internetProviderName;
+    }
+
+    public void setInternetProviderName(String internetProviderName) {
+        this.internetProviderName = internetProviderName;
+    }
+
+    public Double getInternetGBUsed() {
+        return internetGbUsed;
+    }
+
+    public void setInternetGBUsed(Double internetGBUsed) {
+        this.internetGbUsed = internetGBUsed;
+    }
+
+    @Override
+    public Double calculateBill() {
+        this.billAmount = internetGbUsed * 5;
+        return this.billAmount;
+    }
 }
