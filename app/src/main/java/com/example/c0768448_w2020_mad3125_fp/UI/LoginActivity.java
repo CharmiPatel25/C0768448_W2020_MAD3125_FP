b4ae75b321bcff9e8694a46521b4564535c7c6f2
Charmi <c0768448@mylambton.ca>
2020-04-14 18:31:06 -0400

    Inject Butterknife

--- app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/LoginActivity.java
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/LoginActivity.java
@@ -2,11 +2,16 @@ package com.example.c0768448_w2020_mad3125_fp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -24,7 +29,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText _passwordText;
     @InjectView(R.id.btnLogin)
     Button _loginButton;
-
+    private SharedPreferences loginPrefer;
+    private SharedPreferences.Editor loginEditorPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +38,11 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getSupportActionBar().setTitle("Login");
         ButterKnife.inject(this);
-
-        _loginButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });
     }
+
+
+
+
 
 
 }
