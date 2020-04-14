78cb44d292e136c3e0c6b465b607b72821170181
Charmi <c0768448@mylambton.ca>
2020-04-14 19:16:44 -0400

    Make Customer Class implement parceable

--- app/src/main/java/Model/Customer.java
+++ app/src/main/java/Model/Customer.java
@@ -1,4 +1,30 @@
 package Model;
 
-public class Customer {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Customer implements Parcelable {
+    protected Customer(Parcel in) {
+    }
+
+    public static final Creator<Customer> CREATOR = new Creator<Customer>() {
+        @Override
+        public Customer createFromParcel(Parcel in) {
+            return new Customer(in);
+        }
+
+        @Override
+        public Customer[] newArray(int size) {
+            return new Customer[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
 }
