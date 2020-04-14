06790ade7736249f5393853836b204ee74d5c4bd
Charmi <c0768448@mylambton.ca>
2020-04-14 18:17:58 -0400

    find view id login attributes

--- app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/LoginActivity.java
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/LoginActivity.java
@@ -3,15 +3,44 @@ package com.example.c0768448_w2020_mad3125_fp.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 import com.example.c0768448_w2020_mad3125_fp.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @InjectView(R.id.edt_emailID)
+    EditText _emailText;
+    @InjectView(R.id.edt_password)
+    EditText _passwordText;
+    @InjectView(R.id.btnLogin)
+    Button _loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().setTitle("Login");
+        ButterKnife.inject(this);
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
     }
+
+
 }
