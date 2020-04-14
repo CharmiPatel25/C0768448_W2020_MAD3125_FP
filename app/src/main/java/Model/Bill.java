3b14b97efa1f9e55cfd2d35386122de0a55d5a40
Charmi <c0768448@mylambton.ca>
2020-04-14 19:05:06 -0400

    Add Bill Class attributes

--- app/src/main/java/Model/Bill.java
+++ app/src/main/java/Model/Bill.java
@@ -1,4 +1,53 @@
 package Model;
 
-public class Bill {
+import java.util.Date;
+
+public abstract class  Bill {
+public enum BillType{
+    Hydro,Internet,Mobile
+}
+    String billID;
+    Date billDate;
+    BillType billType;
+    double billAmount;
+
+    public Bill(String billID, Date billDate, BillType billType) {
+        this.billID = billID;
+        this.billDate = billDate;
+        this.billType = billType;
+    }
+
+    public String getBillID() {
+        return billID;
+    }
+
+    public void setBillID(String billID) {
+        this.billID = billID;
+    }
+
+    public Date getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(Date billDate) {
+        this.billDate = billDate;
+    }
+
+    public BillType getBillType() {
+        return billType;
+    }
+
+    public void setBillType(BillType billType) {
+        this.billType = billType;
+    }
+
+    public double getBillAmount() {
+        return billAmount;
+    }
+
+    public void setBillAmount(double billAmount) {
+        this.billAmount = billAmount;
+    }
+
+    public abstract Double calculateBill();
 }
