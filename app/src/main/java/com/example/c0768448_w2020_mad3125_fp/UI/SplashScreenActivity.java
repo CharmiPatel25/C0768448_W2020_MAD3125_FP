fd2460b36e778258ab257e3b023d70befaa8ed1d
Charmi <c0768448@mylambton.ca>
2020-04-13 22:01:32 -0400

    Add Splash screen

---
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.c0768448_w2020_mad3125_fp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.c0768448_w2020_mad3125_fp.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+    }
+}
