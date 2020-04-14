c014ca13e3f397c325c350d424f93c611c51dbcf
Charmi <c0768448@mylambton.ca>
2020-04-14 19:11:03 -0400

    Add Hydro Class attributes

--- app/src/main/java/Model/Hydro.java
+++ app/src/main/java/Model/Hydro.java
@@ -1,4 +1,37 @@
 package Model;
 
-public class Hydro {
+import java.util.Date;
+
+public class Hydro extends Bill {
+    private String agencyName;
+    private int unitsConsumed;
+
+    public Hydro(String billID, Date billDate, BillType billType, String agencyName, int unitsConsumed) {
+        super(billID, billDate, billType);
+        this.agencyName = agencyName;
+        this.unitsConsumed = unitsConsumed;
+        this.billAmount = calculateBill();
+    }
+
+    public String getAgencyName() {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    public int getUnitsConsumed() {
+        return unitsConsumed;
+    }
+
+    public void setUnitsConsumed(int unitsConsumed) {
+        this.unitsConsumed = unitsConsumed;
+    }
+
+    @Override
+    public Double calculateBill() {
+        this.billAmount = (double) unitsConsumed * 10.0;
+        return this.billAmount;
+    }
 }
