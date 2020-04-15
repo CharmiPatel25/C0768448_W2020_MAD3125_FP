5795b76fb6d7eccb0e5b783ee9046f54b142c20f
Charmi <c0768448@mylambton.ca>
2020-04-14 22:17:11 -0400

    Add onselected recyclerView event

--- app/src/main/java/Adapter/CustomerListAdapter.java
+++ app/src/main/java/Adapter/CustomerListAdapter.java
@@ -1,13 +1,16 @@
 package Adapter;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.c0768448_w2020_mad3125_fp.R;
+import com.example.c0768448_w2020_mad3125_fp.ShowBillsActivity;
 
 import java.util.ArrayList;
 
@@ -16,9 +19,11 @@ import Model.Customer;
 public class CustomerListAdapter extends RecyclerView.Adapter<CustomerListAdapter.CustomerViewHolder> {
 
     ArrayList<Customer> custList;
+
     public CustomerListAdapter(ArrayList<Customer> custArrayList) {
         this.custList = custArrayList;
-}
+    }
+
     @NonNull
     @Override
     public CustomerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -29,7 +34,23 @@ public class CustomerListAdapter extends RecyclerView.Adapter<CustomerListAdapte
 
     @Override
     public void onBindViewHolder(@NonNull CustomerViewHolder holder, int position) {
+        Customer mCustomer = this.custList.get(position);
+        holder.txtFullName.setText(mCustomer.getFirstName());
+        holder.txtCustID.setText(mCustomer.getCustId());
+        holder.txtEmail.setText(mCustomer.getEmailID());
+        holder.txtDob.setText(mCustomer.getDateOfBirth());
+        holder.txtGender.setText(mCustomer.getGender());
 
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Customer customer = custList.get(position);
+                Intent mIntent = new Intent(holder.itemView.getContext(), ShowBillsActivity.class);
+                mIntent.putExtra("CustomerBills", customer);
+                holder.itemView.getContext().startActivity(mIntent);
+            }
+        });
     }
 
     @Override
@@ -38,8 +59,16 @@ public class CustomerListAdapter extends RecyclerView.Adapter<CustomerListAdapte
     }
 
     public class CustomerViewHolder extends RecyclerView.ViewHolder {
+        TextView  txtFullName, txtCustID,txtEmail,txtDob,txtGender;
+
         public CustomerViewHolder(@NonNull View itemView) {
             super(itemView);
+            this.txtFullName = itemView.findViewById(R.id.txtFullName);
+            this.txtCustID = itemView.findViewById(R.id.txtCustID);
+            this.txtEmail = itemView.findViewById(R.id.txtCustEmail);
+            this.txtGender = itemView.findViewById(R.id.txtCustGender);
+            this.txtDob = itemView.findViewById(R.id.txt_customer_dob);
         }
     }
+}
 
