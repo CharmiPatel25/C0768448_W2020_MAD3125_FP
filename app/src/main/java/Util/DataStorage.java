1f35ec2018d9fec5e38f203ede53b031101ae720
Charmi <c0768448@mylambton.ca>
2020-04-14 20:44:20 -0400

    Add hashmap for customers in dataStorage

--- app/src/main/java/Util/DataStorage.java
+++ app/src/main/java/Util/DataStorage.java
@@ -1,9 +1,27 @@
 package Util;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+import Model.Customer;
+
 public class DataStorage {
     private static DataStorage dataObj = new DataStorage();
     public static DataStorage getInstance() {
         return dataObj;
     }
     private DataStorage() { }
+
+    private HashMap<String, Customer> customers = new HashMap<>();
+
+    public HashMap<String, Customer> getAllCustomerHash() {
+        return this.customers;
+    }
+
+    public ArrayList<Customer> getAllCustomers()
+    {
+        Collection<Customer> tempCustomerList = customers.values();
+        return new ArrayList<>(tempCustomerList);
+    }
 }
