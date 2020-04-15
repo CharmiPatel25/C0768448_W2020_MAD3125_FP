55685ded7789495febeb4fc7b55853cda39f6894
Charmi <c0768448@mylambton.ca>
2020-04-14 21:11:26 -0400

    Create Hydro objects in Data Storage

--- app/src/main/java/Util/DataStorage.java
+++ app/src/main/java/Util/DataStorage.java
@@ -1,10 +1,14 @@
 package Util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
+import Model.Bill;
 import Model.Customer;
+import Model.Hydro;
 
 public class DataStorage {
     private static DataStorage dataObj = new DataStorage();
@@ -25,8 +29,9 @@ public class DataStorage {
         return new ArrayList<>(tempCustomerList);
     }
 
-    public void loadData()
-    {
+    public void loadData() throws ParseException {
+        SimpleDateFormat formatter1=new SimpleDateFormat("dd/MM/yyyy");
+
         Customer c1 = new Customer("C001","Charmi","Patel","charmi@gmail.com","Female","25/11/1993");
         Customer c2 = new Customer("C002","Avani","Patel","avani@gmail.com","Female","5/01/1996");
         Customer c3 = new Customer("C003","Jyothi","Thomas","jyothi@gmail.com","Female","24/10/1991");
@@ -38,5 +43,9 @@ public class DataStorage {
         customers.put(c3.getCustId(),c3);
         customers.put(c4.getCustId(),c4);
         customers.put(c5.getCustId(),c5);
+        Hydro h1 = new Hydro("HD001",formatter1.parse("12/03/2019"), Bill.BillType.Hydro,"Tata",45);
+        Hydro h2 = new Hydro("HD002",formatter1.parse("25/02/2019"), Bill.BillType.Hydro,"Reliance",20);
+
+
     }
 }
