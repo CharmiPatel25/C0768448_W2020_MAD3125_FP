fdc166814c9ff252692ee411faf91d0fea87b7f7
Charmi <c0768448@mylambton.ca>
2020-04-14 20:49:25 -0400

    Create customer objects in data repository

--- app/src/main/java/Util/DataStorage.java
+++ app/src/main/java/Util/DataStorage.java
@@ -24,4 +24,15 @@ public class DataStorage {
         Collection<Customer> tempCustomerList = customers.values();
         return new ArrayList<>(tempCustomerList);
     }
+
+    public void loadData()
+    {
+        Customer c1 = new Customer("C001","Charmi","Patel","charmi@gmail.com","Female","25/11/1993");
+        Customer c2 = new Customer("C002","Avani","Patel","avani@gmail.com","Female","5/01/1996");
+        Customer c3 = new Customer("C003","Jyothi","Thomas","jyothi@gmail.com","Female","24/10/1991");
+        Customer c4 = new Customer("C004","Jon","Snow","jon@gmail.com","Male","10/03/1989");
+        Customer c5 = new Customer("C005","Bran","Stark","bran@gmail.com","Male","19/02/1998");
+
+
+    }
 }
