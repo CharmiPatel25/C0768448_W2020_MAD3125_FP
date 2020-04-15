fe904e2c6c9afe7d3a44b85e1476e1e56dcbc5b2
Charmi <c0768448@mylambton.ca>
2020-04-14 20:40:35 -0400

    Create Singleton Class for data repository

--- app/src/main/java/Util/DataStorage.java
+++ app/src/main/java/Util/DataStorage.java
@@ -1,4 +1,9 @@
 package Util;
 
 public class DataStorage {
+    private static DataStorage dataObj = new DataStorage();
+    public static DataStorage getInstance() {
+        return dataObj;
+    }
+    private DataStorage() { }
 }
