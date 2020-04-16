1b134404d904f7dba1b4685187cde381f44d8b92
Charmi <c0768448@mylambton.ca>
2020-04-15 22:38:31 -0400

    Add new customers attributed

--- app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/AddNewCustomerActivity.java
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/AddNewCustomerActivity.java
@@ -2,11 +2,21 @@ package com.example.c0768448_w2020_mad3125_fp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 import com.example.c0768448_w2020_mad3125_fp.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AddNewCustomerActivity extends AppCompatActivity {
+    private TextInputLayout lblCustID, lblFirstName, lblLastName, lblBirthDate, lblEmailID;
+    private RadioButton rdBMale, rdFemale, rdOther;
+    private TextInputEditText edtTxtCustID, edtTxtFirstName, edtTxtLastName, edtTxtBirthDate,edtTxtEmailID;
+    private Button btnAdd, btnClear;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
