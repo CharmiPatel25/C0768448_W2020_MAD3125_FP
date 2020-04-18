b4617bd1cf87a2479054dfd63fe352336af1697a
Charmi <c0768448@mylambton.ca>
2020-04-18 17:39:26 -0400

    Add fragments for hydro,internet,mobile bills display

--- app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/BillTypeDetailsActivity.java
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/BillTypeDetailsActivity.java
@@ -12,6 +12,7 @@ public class BillTypeDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_type_details);
-        getSupportActionBar().setTitle("Bill Details");
+        getSupportActionBar().setTitle("Bill Information");
+
     }
 }
