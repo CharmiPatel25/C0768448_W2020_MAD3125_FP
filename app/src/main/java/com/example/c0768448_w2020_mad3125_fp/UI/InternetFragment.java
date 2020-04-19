0255136e10a2832c6a5408694120dceedcb4e7e5
Charmi <c0768448@mylambton.ca>
2020-04-19 11:28:59 -0400

    Add function for internet fragment

--- app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/InternetFragment.java
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/InternetFragment.java
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.c0768448_w2020_mad3125_fp.Model.Bill;
 import com.example.c0768448_w2020_mad3125_fp.R;
 
 
@@ -19,13 +21,13 @@ import com.example.c0768448_w2020_mad3125_fp.R;
 public class InternetFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+   // private static final String ARG_PARAM1 = "param1";
+   // private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+   // private String mParam1;
+   // private String mParam2;
+    Bill bill;
     public InternetFragment() {
         // Required empty public constructor
     }
@@ -39,28 +41,38 @@ public class InternetFragment extends Fragment {
      * @return A new instance of fragment InternetFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static InternetFragment newInstance(String param1, String param2) {
+  /*  public static InternetFragment newInstance(String param1, String param2) {
         InternetFragment fragment = new InternetFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
+    }*/
 
-    @Override
+ /*   @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
+    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+      //  return inflater.inflate(R.layout.fragment_internet, container, false);
+        Bundle bundle = new Bundle();
+        try
+        {
+            bill     = (Bill) bundle.getSerializable("internetObj");
+        }
+        catch (NullPointerException e)
+        {
+            Toast.makeText(getActivity(), "Internet Bill not generated", Toast.LENGTH_SHORT).show();
+        }
         return inflater.inflate(R.layout.fragment_internet, container, false);
     }
 }
