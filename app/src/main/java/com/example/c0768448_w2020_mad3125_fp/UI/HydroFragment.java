e13f0c08334ff1d25cc85306570a6a16cabf8c1d
Charmi <c0768448@mylambton.ca>
2020-04-19 11:27:02 -0400

    Add function for hydro fragment

--- app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/HydroFragment.java
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_fp/UI/HydroFragment.java
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.c0768448_w2020_mad3125_fp.Model.Bill;
 import com.example.c0768448_w2020_mad3125_fp.R;
 
 
@@ -19,12 +21,7 @@ import com.example.c0768448_w2020_mad3125_fp.R;
 public class HydroFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Bill bill;
 
     public HydroFragment() {
         // Required empty public constructor
@@ -39,28 +36,38 @@ public class HydroFragment extends Fragment {
      * @return A new instance of fragment HydroFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HydroFragment newInstance(String param1, String param2) {
+  /*  public static HydroFragment newInstance(String param1, String param2) {
         HydroFragment fragment = new HydroFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
+    }*/
 
-    @Override
+   /* @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
+    } */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+       // return inflater.inflate(R.layout.fragment_hydro, container, false);
+        Bundle bundle = new Bundle();
+        try
+        {
+            bill = (Bill) bundle.getSerializable("hydroBill");
+        }
+        catch (NullPointerException e)
+        {
+            Toast.makeText(getActivity(), "Hydro Bill not generated", Toast.LENGTH_SHORT).show();
+        }
         return inflater.inflate(R.layout.fragment_hydro, container, false);
     }
 }
